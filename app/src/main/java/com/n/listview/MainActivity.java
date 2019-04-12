1a40b7377160a8ef5c709a269507d8e67bf5e39d
Softwarica College Lab <softwarica@softwarica.com>
2019-04-12 08:12:45 +0545

    ArrayAdapter, hashmap

--- app/src/main/java/com/n/listview/MainActivity.java
+++ app/src/main/java/com/n/listview/MainActivity.java
@@ -2,12 +2,52 @@ package com.n.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.nio.MappedByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+  ListView lstcountries;
+    public static final String countries[]={
+            "Nepal","kathmandu",
+            "India","New Delhi",
+            "china","Beijing",
+            "USA","Washington,dc",
+            "UK","london"
+    };
+    private Map<String,String>dictionary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lstcountries=findViewById(R.id.lstcountries);
+
+        dictionary=new HashMap<>();
+        for(int i=0;i<countries.length;i+=2){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
+        ArrayAdapter countryAdapter=new ArrayAdapter<>(
+                this,android.R.layout.simple_list_item_1,
+                new ArrayList<String>(dictionary.keySet())
+
+        );
+        lstcountries.setAdapter(countryAdapter);
+
+        lstcountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country =parent.getItemAtPosition(position).toString();
+                String capital=dictionary.get(country);
+                Toast.makeText(getApplicationContext(),capital.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
